ed20afd48bae11fad55e326945e6ea3afa273ad2
Konyancha1 <kmutua791@gmail.com>
2024-01-27 12:03:14 +0200

    Changes to config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+uploads_folder = os.path.join(current_directory, 'uploads')
+
+app.config['UPLOAD_FOLDER'] = uploads_folder
 
 from app import routes
 
--- app/model_utils.py
+++ app/model_utils.py
@@ -1,13 +1,31 @@
 import cv2
 import numpy as np
+import os
+import keras
 from keras.models import load_model
+from tempfile import NamedTemporaryFile
+
+def iou_coef(y_true, y_pred, smooth=1):
+    intersection = np.sum(np.abs(y_true * y_pred), axis=[1, 2, 3])
+    union = np.sum(y_true, axis=[1, 2, 3]) + np.sum(y_pred, axis=[1, 2, 3]) - intersection
+    iou = np.mean((intersection + smooth) / (union + smooth), axis=0)
+    return iou
 
 # Load your trained segmentation model
 def load_segmentation_model():
-    # Adjust the path to your trained model file
-    model_path = 'JAN2024HACKATHON\model (1).h5'
-    model = load_model(model_path)
-    return model
+    try:
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        model_path = os.path.join(current_directory, 'model (1).h5')
+
+        with keras.utils.custom_object_scope({'iou_coef': iou_coef}):
+            model = load_model(model_path)
+
+        print("Model loaded successfully.")
+        return model
+
+    except Exception as e:
+        print("Error loading the model:", str(e))
+        return None
 
 # Preprocess the input image for segmentation
 def preprocess_image(image_path):
@@ -33,4 +51,9 @@ def process_image(image_path):
     # Perform segmentation prediction
     segmentation_result = segmentation_model.predict(input_image)
 
-    return segmentation_result
+    # Save the segmented image to a temporary file
+    with NamedTemporaryFile(delete=False, suffix='.png') as temp_file:
+        # Assuming segmentation_result is an image, adjust the saving accordingly
+        cv2.imwrite(temp_file.name, segmentation_result[0] * 255)
+
+    return temp_file.name
